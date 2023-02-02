f2b03a85cef646aa035ffcdd80ceb57082c2f477
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-02-02 10:38:31 +0900

    removed unncessary code form measures

--- measures/i_index.py
+++ measures/i_index.py
@@ -6,22 +6,9 @@ import copy
 
 
 def i_index(X, labels):
-	"""
-	Calculate the I index of a clustering.
-		:param X: The data matrix.
-		:param label: The cluster labels.
-		:return: The I index
-	Note that the power constant is set to 2, following the original paper where
-	I index have been first introduced (Maulik at al. TPAMI 2002)
-	"""
 	n_clusters = len(np.unique(labels))
 	n_samples = X.shape[0]
 	n_features = X.shape[1]
-
-	"""
-	compute the compactness of each cluster as relative distance to the centroid of the cluster
-	to the centroid of the whole data set
-	"""
 
 	## compute the centroids of each cluster
 	centroids= np.zeros((n_clusters, n_features))
@@ -46,9 +33,6 @@ def i_index(X, labels):
 	### compute compactness
 	compactness =  (dist_sum_whole / dist_sum_to_centroids) / n_clusters
 
-	"""
-	compute the separability as max distance between centroids
-	"""
 	max_dist = 0
 	for i in range(n_clusters):
 		for j in range(i + 1, n_clusters):
@@ -132,7 +116,6 @@ def i_index_shift(X, labels):
 	max_centroid_dist = 0
 	for i in range(n_clusters):
 		for j in range(i + 1, n_clusters):
-			# dist = np.sum(np.square(centroids[i, :] - centroids[j, :])) 
 			dist = np.sqrt(np.sum(np.square(centroids[i, :] - centroids[j, :])))
 			if dist > max_centroid_dist:
 				max_centroid_dist = dist
@@ -146,77 +129,16 @@ def i_index_shift(X, labels):
 
 	return results 
 
-# def i_index_shift_exp(X, labels):
-# 	_clusters = len(np.unique(labels))
-# 	n_samples = X.shape[0]
-# 	n_features = X.shape[1]
 
-# 	std = np.std(np.sqrt(np.sum(np.square(X - utils.centroid(X)), axis=1)))
-
-# 	## compute the centroid of the whole data set
-# 	centroid_whole = utils.centroid(X)
-
-# 	## compute the sum of distances to the centroid of the whole data set
-# 	dist_squared_whole = np.square(X - centroid_whole)
-# 	dist_row_sum_whole = np.sqrt(np.sum(dist_squared_whole, axis=1))
-# 	dist_sum_whole = np.sum(np.exp(dist_row_sum_whole / std))
-
-def i_index_shift_exp(X):
-	# n_samples = X.shape[0]
-	# n_features = X.shape[1]
-
-
-	# std = np.std(np.sum(np.square(X - utils.centroid(X)), axis=1))
-	# 	## compute the centroids of each cluster
-	# centroids= np.zeros((n_clusters, n_features))
-	# for i in range(n_clusters):
-	# 	centroids[i, :] = utils.centroid(X[labels == i, :])
-
-
-	# ## compute the centroids of each cluster
-	# centroid_whole = utils.centroid(X)
-	# dist_squared_whole = np.square(X - centroid_whole)
-	# dist_row_sum_whole = np.sqrt(np.sum(dist_squared_whole, axis=1))
-	# dist_sum_whole = np.sum(np.exp(dist_row_sum_whole / std))
-	# avg_dist_whole = dist_sum_whole / n_samples
-
-	# results = (dist_sum_whole / (((avg_dist_whole) + 1)))
-	# return results
-	return 
 
 def i_index_shift_range(X, labels, k):
 	orig = i_index_shift(X, labels)
 	orig_logistic = 1 / (1 + np.exp(-k * orig))
-
 	e_val = 0
-	
-	# e_val = 1
 	e_val_logistic = 1 / (1 + np.exp(-k * e_val))
 
-
-	# print()
-	# print(orig, e_val)
-
-	# if e_val_logistic == 1:
-	# 	return 0
-	
-
-	# print((orig_logistic - e_val_logistic) / (1 - e_val_logistic))
 	return (orig_logistic - e_val_logistic) / (1 - e_val_logistic)
 
-	# return orig_logistic
-	# e_val_sum = 0
-	# for i in range(20):
-	# 	np.random.shuffle(labels)
-	# 	e_val_sum += i_index_shift(X, labels)
-	# e_val = e_val_sum / 20
-	# print(e_val)
-	
-	# e_val = 0.25
-	
-	# e_val_logistic = 1 / (1 + e_val ** (-1))
-	# print(orig_logistic, e_val_logistic)
-	# return (orig_logistic - e_val_logistic) / (1 - e_val_logistic)
 
 def i_index_shift_range_class(X, labels, k):
 	return utils.pairwise_computation_k(X, labels, k, i_index_shift_range)
--- measures/silhouette.py
+++ measures/silhouette.py
@@ -12,28 +12,22 @@ def silhouette(X, labels):
 def a_exp_value(X, labels, curr_label, index, std):
 	curr_cluster = X[labels == curr_label, :]
 	curr_point   = X[index]
-	# dist_sum = np.sum(np.exp(np.sqrt(np.sum(np.square(curr_cluster - curr_point), axis=1))) ** (1 / std))
 	dist_mean = np.sum(np.sqrt(np.sum(np.square(curr_cluster - curr_point), axis=1))) / (curr_cluster.shape[0] - 1)
-	
-	# return np.sum(dist_sum) / (curr_cluster.shape[0] - 1)
 
 	return dist_mean
 
 def b_exp_value(X, labels, curr_label, index, n_clusters, std):
 	curr_point = X[index]
-	# min_dist_sum = np.inf
 	min_dist_mean = np.inf
 	cluster_size = np.inf
 	for cluster_label in range(n_clusters):
 		if cluster_label != curr_label:
 			cluster = X[labels == cluster_label, :]
-			# dist_sum = np.sum(np.exp(np.sqrt(np.sum(np.square(cluster - curr_point), axis=1))) ** (1 / std))
 			dist_mean = np.sum(np.sqrt(np.sum(np.square(cluster - curr_point), axis=1))) / cluster.shape[0]
 			if dist_mean < min_dist_mean:
 				min_dist_mean = dist_mean
 	
 	return min_dist_mean
-## Silhouette_Range does not need to be implemented -- already satisfies range invrainct
 
 def silhouette_shift(X, labels):
 	n_clusters = len(np.unique(labels))
@@ -53,12 +47,6 @@ def silhouette_shift(X, labels):
 			silhouette_list_zero.append(denominator / numerator)
 		else:
 			silhouette_list_first.append(denominator / numerator)
-			# a_val_exp = a_exp_value(X, labels, labels[i], i, std)
-		# b_val_exp = b_exp_value(X, labels, labels[i], i, n_clusters, std)
-		# if (labels[i] == 0):
-		# 	silhouette_list_zero.append((b_val_exp - a_val_exp) / max(a_val_exp, b_val_exp))
-		# else:
-		# 	silhouette_list_first.append((b_val_exp - a_val_exp) / max(a_val_exp, b_val_exp))
 	value = (np.mean(silhouette_list_zero) + np.mean(silhouette_list_first)) / 2
 	return value
 
